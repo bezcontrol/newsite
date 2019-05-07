5183d98d72c8c0febbf0298fa0a9f0be2872de9e
bezcontrol <oleksii.baklanov@nure.ua>
2019-05-08 00:27:39 +0300

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+app.use('/assets', express.static('assets'))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
